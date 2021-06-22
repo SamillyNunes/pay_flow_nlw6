d1ff5d924d134e13688de8d6f42894a872349b7f
SamillyNunes <samillynunes19@gmail.com>
2021-06-22 16:47:13 -0300

    Splash page created

---
+++ lib/modules/splash/splash_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:payflow_nlw6/core/app_colors.dart';
+import 'package:payflow_nlw6/core/app_images.dart';
+
+class SplashPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: Stack(
+        children: [
+          Center(
+            child: Image.asset(
+              AppImages.union,
+            ),
+          ),
+          Center(
+            child: Image.asset(
+              AppImages.logoFull,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
