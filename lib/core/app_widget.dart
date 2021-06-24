bc2b9d88fcec36f07fbaa3465252ff0273f72222
SamillyNunes <samillynunes19@gmail.com>
2021-06-24 11:33:37 -0300

    Locking orientation

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:payflow_nlw6/modules/barcode_scanner/barcode_scanner_page.dart';
 import 'package:payflow_nlw6/modules/home/home_page.dart';
 import 'package:payflow_nlw6/modules/login/login_page.dart';
@@ -7,7 +8,14 @@ import 'package:payflow_nlw6/modules/splash/splash_page.dart';
 import 'app_colors.dart';
 
 class AppWidget extends StatelessWidget {
-  // This widget is the root of your application.
+  AppWidget() {
+    // para bloquear a rotação do app
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp,
+    ]);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
